853aafb46b30cfa2a170ed60721dcc1bc12a0855
safwan-ms <safwan.anasms@gmail.com>
2025-03-25 14:35:41 +0530

    feat: complete deployment setup with environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./database/db.js";
 import dotenv from "dotenv";
+import path from "path";
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
@@ -9,11 +10,20 @@ connectDB();
 const port = process.env.PORT;
 const app = express();
 
+const __dirname = path.resolve();
+
 //Middleware
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(port, () =>
   console.log(`Server is listening to http://localhost:${port}`)
 );
